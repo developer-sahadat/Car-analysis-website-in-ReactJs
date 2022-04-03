04f2c5da861e44ec215d3eb6d72be81a158559eb
developer-sahadat <mdridoysikder862@gmail.com>
2022-04-03 23:56:20 +0600

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import About from './Components/About/About';
+import Dashboards from './Components/Dashboards/Dashboards';
 import Header from './Components/Header/Header';
+import HomePage from './Components/HomePage/HomePage';
+import Reviews from './Components/Reviews/Reviews';
 
 function App() {
   return (
     <div>
       <Header/>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/reviews' element={<Reviews/>} />
+        <Route path='/dashboard' element={<Dashboards/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Dashboards/Dashboards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboards = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Dashboards;
