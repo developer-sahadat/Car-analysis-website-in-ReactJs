969ad66ff6f1cc412443dd83aa54ffb05300e279
developer-sahadat <mdridoysikder862@gmail.com>
2022-04-03 23:47:22 +0600

    navbar added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='shadow-sm'>
+            <Navbar  bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Car Shop</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/reviews">Reviews</Nav.Link>
+                        <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>  
         </div>
     );
 };
